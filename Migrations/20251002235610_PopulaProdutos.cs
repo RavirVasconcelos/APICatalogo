153021a8de8d4c313804aa524d871cc88334e04d
Ytalo Ravir <ytalovasconcelos01@gmail.com>
2025-10-02 21:02:11 -0300

    Feito o ajuste de tabelas e popular as tabelas pelo migrations

---
+++ Migrations/20251002235610_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " Values('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " Values('HotDog', 'HotDog de Salsicha', 3.42, 'hotdog.jpg', 20, now(), 2)");
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+                " Values('Sorvete', 'Sorvete de Flocos', 7.58, 'sorvete.jpg', 41, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,26 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
     //Atenção: como será mapeado para o banco de dados, é necessário ter a propriedade Id primeiro
+
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
 }
